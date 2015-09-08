1856573abb555911d2e535304be0ce1ad78e3228
kazuyukitanimura <kazuyukitanimura@gmail.com>
2015-09-07 17:53:22 -0700

    pre-commit

--- getIntraday.js
+++ getIntraday.js
@@ -4,8 +4,7 @@ var INTERVAL = 60; // sec
 var PERIOD = 10; // days
 var TICKER = 'GOOG';
 
-var GoogleCSVReader function(data) {
-};
+var GoogleCSVReader = function(data) {};
 
 var url = ['http://www.google.com/finance/getprices?i=', INTERVAL, '&p=', PERIOD, 'd&f=d,o,h,l,c,v&df=cpct&q=', TICKER.toUpperCase()].join('');
 rest.get(url).on('success', function(data) {
