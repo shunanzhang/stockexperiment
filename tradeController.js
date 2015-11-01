2d11d7bd3f8c1c7562293e278519c2b34843c22e
kazuyukitanimura <kazuyukitanimura@gmail.com>
2015-10-31 22:16:18 -0700

    sell bug fix

--- tradeController.js
+++ tradeController.js
@@ -37,11 +37,11 @@ var TradeController = module.exports = function(columns, closes) {
   this.lowColumnIndex = columns[LOW_COLUMN];
   this.openColumnIndex = columns[OPEN_COLUMN];
   this.volumeColumnIndex = columns[VOLUME_COLUMN];
-  var reverseCloses = [];
-  for (var i = columns.length; i--;) {
-    reverseCloses[i] = 10000000 - closes[i];
-  }
-  this.kMaximalLosses = new KMaximalGains(reverseCloses);
+  //var reverseCloses = [];
+  //for (var i = closes.length; i--;) {
+  //  reverseCloses[i] = 10000000 - closes[i];
+  //}
+  //this.kMaximalLosses = new KMaximalGains(reverseCloses);
 };
 TradeController.BUY = BUY;
 TradeController.SELL = SELL;
@@ -70,14 +70,14 @@ TradeController.prototype.trade = function(featureVector, forceHold) {
 
 TradeController.prototype.supervise = function(i) {
   this.kMaximalGains.getOptimal(K, i - TRAIN_LEN + 1, i);
-  this.kMaximalLosses.getOptimal(K, i - TRAIN_LEN + 1, i);
+  //this.kMaximalLosses.getOptimal(K, i - TRAIN_LEN + 1, i);
 };
 
 TradeController.prototype.train = function(i_j, featureVector) {
   var correctResult = this.kMaximalGains.isInRange(i_j, BUY, HOLD);
-  if (correctResult === HOLD) {
-    correctResult = this.kMaximalLosses.isInRange(i_j, SELL, HOLD);
-  }
+  //if (correctResult === HOLD) {
+  //  correctResult = this.kMaximalLosses.isInRange(i_j, SELL, HOLD);
+  //}
   this.scw.update({
     featureVector: featureVector,
     category: correctResult
