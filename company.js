9587ce12e6383684c9ca325a57fad515118f9639
kazuyukitanimura <kazuyukitanimura@gmail.com>
2016-05-06 09:19:11 -0700

    206.36 204.64

--- company.js
+++ company.js
@@ -14,11 +14,11 @@ var EXCHANGES = {
 };
 
 var MIN_PRICES = {
-  SPY:  205.64
+  SPY:  204.64
 };
 
 var MAX_PRICES = {
-  SPY:  205.36
+  SPY:  206.36
 };
 
 var MAX_LOTS = {
@@ -26,7 +26,7 @@ var MAX_LOTS = {
 };
 
 var HARD_L_MAX_LOTS = {
-  SPY: 3
+  SPY: 4
 };
 
 var HARD_S_MAX_LOTS = {
