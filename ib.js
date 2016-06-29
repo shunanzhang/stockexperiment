baf9a325b6f63fe6980c98d772e89131ee09cae8
kazuyukitanimura <kazuyukitanimura@gmail.com>
2016-06-29 08:04:48 -0700

    no setImmediate

--- ib.js
+++ ib.js
@@ -65,7 +65,7 @@ var placeMyOrder = function(company, action, quantity, orderType, lmtPrice, auxP
   newOrder.tif = 'GTC';
   newOrder.outsideRth = true;
   newOrder.percentOffset = 0; // bug workaround
-  setImmediate(api.placeOrder.bind(api, oldId, company.contract, newOrder));
+  api.placeOrder(oldId, company.contract, newOrder);
   //console.log('Next valid order Id: %d', oldId);
   console.log('Placing order for', company.symbol, newOrder, company.bid, company.ask);
 };
