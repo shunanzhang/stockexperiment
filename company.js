b72998bd5130969643b40df4d79e568bbf11a5f0
kazuyukitanimura <kazuyukitanimura@gmail.com>
2016-09-23 09:03:42 -0700

    adjust

--- company.js
+++ company.js
@@ -42,7 +42,7 @@ var HARD_L_MIN_PERCENTS = {
 
 var HARD_S_MIN_PERCENTS = {
   SPY: [0.961, 0.971, 0.981],
-  ES: [0.976, 0.991, 1.001]
+  ES: [0.976, 0.986, 0.996]
 };
 
 var HARD_S_MAX_PERCENTS = {
