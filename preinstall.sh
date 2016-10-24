cec07e1cb3f0bba425ec34e498e39f6b8dcb138e
kazuyukitanimura <kazuyukitanimura@gmail.com>
2016-10-23 18:49:47 -0700

    build fix for linux

--- preinstall.sh
+++ preinstall.sh
@@ -1,3 +1,6 @@
 rm -rf IBJts
 wget -c http://interactivebrokers.github.io/downloads/twsapi_macunix.971.01.jar
 unzip twsapi_macunix.971.01.jar
+
+# http://stackoverflow.com/questions/5694228/sed-in-place-flag-that-works-both-on-mac-bsd-and-linux
+sed -i.bak 's/\/\/ LINUX/#include <unistd.h>/' IBJts/source/PosixClient/src/EPosixClientSocketPlatform.h
